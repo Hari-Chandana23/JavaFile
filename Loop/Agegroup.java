3e4cdd17ef320f52e488b76f1ce90bfaa527964b
Hari-Chandana23 <harichandana2123@gmail.com>
2024-01-17 10:39:55 -0600

    Executed loop conditions

---
+++ Loop/Agegroup.java
@@ -0,0 +1,21 @@
+package Loop;
+
+public class Agegroup {
+
+        static String getAgegroup(int age){
+            if(age > 13 && age < 19)
+                return "Teen";
+            else if( age < 13)
+                return "Kid";
+            else
+                return "Adult";
+
+
+        }
+
+        public static void main(String[] args) {
+            System.out.println(getAgegroup(12));
+        }
+    }
+
+
